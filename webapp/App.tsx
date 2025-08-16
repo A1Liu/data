b02c997c28e624ff2dfbd9b10e8937bb50d60924
Albert Liu <albertymliu@gmail.com>
2025-08-16 18:38:00 -0400

    adding more router code

--- webapp/App.tsx
+++ webapp/App.tsx
@@ -1,25 +1,66 @@
 import { useState } from "react";
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import "./App.css";
+import {
+  Outlet,
+  RouterProvider,
+  Link,
+  createRouter,
+  createRoute,
+  createRootRoute,
+} from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+
+export const rootRoute = createRootRoute({
+  component: () => (
+    <>
+      <div className="p-2 flex gap-2">
+        <Link to="/" className="font-light [&.active]:font-bold">
+          Home
+        </Link>{" "}
+        <Link to="/ranges" className="font-light [&.active]:font-bold">
+          Ranges
+        </Link>
+      </div>
+      <hr />
+
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  ),
+});
+
+const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/",
+  component: function Index() {
+    const [count, setCount] = useState(0);
+
+    return (
+      <>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+      </>
+    );
+  },
+});
+
+const routeTree = rootRoute.addChildren([indexRoute]);
+
+export const Router = createRouter({ routeTree });
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={Router} />
     </>
   );
 }
